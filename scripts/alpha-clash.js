b10e7a77fc0341bd5fe312b4d4092edea1b8e3d0
kibriarobin97 <kibriarobin97@gmail.com>
2024-02-15 02:12:28 +0600

    added playground js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -9,6 +9,54 @@
 //     playGround.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event) {
+    const playerPressed = event.key;
+
+    // get the expected key press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+
+    // checked matched or not
+    if (playerPressed === expectedAlphabet) {
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+        //------------------------------another way-------------------
+        // score update
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // const newScore = currentScore + 1;
+
+        // // show the update score
+        // currentScoreElement.innerText = newScore;
+
+        // new round start
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        //------------------------another way-----------------
+        // get the current life
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // // updated life
+        // const newLife = currentLife - 1;
+
+        // // display life
+        // currentLifeElement.innerText = newLife;
+    }
+}
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame() {
     const alphabet = getRandomAlphabet();
 
